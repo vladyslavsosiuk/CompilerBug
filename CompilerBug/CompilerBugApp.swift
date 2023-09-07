739e828b6380ff34642becc44484071caeca6f98
Vladyslav Sosiuk <vladyslav.sosiuk@raymondjames.com>
2023-09-07 08:44:39 -0700

    Initial Commit

---
+++ CompilerBug/CompilerBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompilerBugApp.swift
+//  CompilerBug
+//
+//  Created by Vladyslav Sosiuk on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CompilerBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
